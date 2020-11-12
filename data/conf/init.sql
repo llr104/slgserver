60d0ed8a30190789db5c2534ea81d5a43657e15e
llr104 <lianglirong1991@gmail.com>
2020-11-12 16:48:58 +0800

    Update init.sql

--- data/conf/init.sql
+++ data/conf/init.sql
@@ -1,5 +1,25 @@
 INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(1, 1, "城区", 0, 0, 0, 0, 1000, 5);
+
 INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(2, 1, "木Lv.1", 1000, 100, 100, 0, 100, 1);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(2, 2, "木Lv.2", 2000, 200, 200, 0, 200, 2);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(2, 3, "木Lv.3", 3000, 300, 300, 0, 300, 3);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(2, 4, "木Lv.4", 4000, 400, 400, 0, 400, 4);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(2, 5, "木Lv.5", 5000, 500, 500, 0, 500, 5);
+
 INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(3, 1, "铁Lv.1", 100, 1000, 100, 0, 100, 1);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(3, 2, "铁Lv.2", 200, 2000, 200, 0, 200, 2);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(3, 3, "铁Lv.3", 300, 3000, 300, 0, 300, 3);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(3, 4, "铁Lv.4", 400, 4000, 400, 0, 400, 4);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(3, 5, "铁Lv.5", 500, 5000, 500, 0, 500, 5);
+
 INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(4, 1, "石Lv.1", 0, 100, 1000, 100, 100, 1);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(4, 2, "石Lv.2", 0, 200, 2000, 200, 200, 2);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(4, 3, "石Lv.3", 0, 300, 3000, 300, 300, 3);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(4, 4, "石Lv.4", 0, 400, 4000, 400, 400, 4);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(4, 5, "石Lv.5", 0, 500, 5000, 500, 500, 5);
+
 INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(5, 1, "田Lv.1", 0, 100, 100, 1000, 100, 1);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(5, 2, "田Lv.2", 0, 200, 200, 2000, 200, 2);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(5, 3, "田Lv.3", 0, 300, 300, 3000, 300, 3);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(5, 4, "田Lv.4", 0, 400, 400, 4000, 400, 4);
+INSERT INTO `build_config`(`type`, `level`, `name`, `wood`, `iron`, `stone`, `grain`, `durable`, `defender`) VALUES(5, 5, "田Lv.5", 0, 500, 500, 5000, 500, 5);
