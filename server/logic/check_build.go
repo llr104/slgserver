af24b0dc908fec788e96b881b12748ecacb3c87b
llr104 <lianglirong1991@gmail.com>
2020-12-09 15:54:50 +0800

    是否可到达bug修改

--- server/logic/check_build.go
+++ server/logic/check_build.go
@@ -2,11 +2,7 @@ package logic
 
 import "slgserver/server/global"
 
-//是否能到达
-func IsCanArrive(x, y, rid int) bool {
-	unionId := RAttributeMgr.UnionId(rid)
-
-	//先判断上下左右是否有普通地相连
+func hasRoleBuildNearBy(x, y, rid, unionId int) bool {
 	for i := x-1; i <= x+1; i++ {
 		if i < 0 || i >= global.MapWith{
 			continue
@@ -26,13 +22,52 @@ func IsCanArrive(x, y, rid int) bool {
 			}
 		}
 	}
+	return false
+}
 
-	//再判断是否和城市相连， 因为城池占了9格，所以该格子附近两个格子范围内有城池，则该地方是城池
-	for i := x-2; i <= x+2; i++ {
-		for j := y-2; j <= y+2; j++ {
-			if rc, ok := RCMgr.PositionCity(i, j); ok {
-				if rc.RId == rid || (unionId != 0 && rc.UnionId == unionId) {
-					return true
+//是否能到达
+func IsCanArrive(x, y, rid int) bool {
+	unionId := RAttributeMgr.UnionId(rid)
+
+	//目标位置是城池
+	if _, ok := RCMgr.PositionCity(x, y); ok {
+		//城的四周是否有地相连
+		//上
+		ok := hasRoleBuildNearBy(x, y+2, rid, unionId)
+		if ok {
+			return ok
+		}
+
+		//下
+		ok = hasRoleBuildNearBy(x, y-2, rid, unionId)
+		if ok {
+			return ok
+		}
+
+		//左
+		ok = hasRoleBuildNearBy(x-2, y, rid, unionId)
+		if ok {
+			return ok
+		}
+
+		ok = hasRoleBuildNearBy(x+2, y, rid, unionId)
+		if ok {
+			return ok
+		}
+	}else{
+		//普通领地
+		ok := hasRoleBuildNearBy(x, y, rid, unionId)
+		if ok {
+			return true
+		}
+
+		//再判断是否和城市相连， 因为城池占了9格，所以该格子附近两个格子范围内有城池，则该地方是城池
+		for i := x-2; i <= x+2; i++ {
+			for j := y-2; j <= y+2; j++ {
+				if rc, ok := RCMgr.PositionCity(i, j); ok {
+					if rc.RId == rid || (unionId != 0 && rc.UnionId == unionId) {
+						return true
+					}
 				}
 			}
 		}
